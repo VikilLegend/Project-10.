dd05ccae4cee1ae6e0fe25efd784d18ed86076a6
VikilLegend <88488370+VikilLegend@users.noreply.github.com>
2021-08-10 12:26:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+
+function preload(){
+seaImg = loadImage("sea.png");
+
+shipImg1 = loadImage("ship-1.png","ship-2.png","ship-3.png","ship-4.png");
+
+}
+
+function setup(){
+  createCanvas(400,400); 
+sea = createSprite;
+sea.addImage(seaImg);
+}
+
+function draw() {
+  background("blue");
+ 
+}
